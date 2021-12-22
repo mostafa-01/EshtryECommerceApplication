873b6b53f8dacd19111d8d334898f467f28ae4db
AndrewAtef17 <58080394+AndrewAtef17@users.noreply.github.com>
2021-12-22 03:59:25 +0200

    Merged the frontend progress

---
+++ EshtryFrontend/index.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ecommerce._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="CSS/styles.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div class="navbar">
+            <a href="#home">Home</a>
+            <a style="float:right" href="#Login">Login</a>
+            <a style="float:right" href="#Register">Register</a>
+       </div>
+       <div class="main">
+           <div class="example">
+          <input type="text" placeholder="Search.." name="search2"/>
+          <asp:DropDownList style="height:42px;" Class="button" ID="DropDownList1" runat="server"> </asp:DropDownList>
+          <asp:button  Class="button"  runat="server" Text="Search"></asp:button>  
+          &nbsp;</div>
+        <asp:Panel ID="pnl" CssClass="parent" runat="server">
+            </asp:Panel>
+
+          
+       </div>
+       
+    </form>
+</body>
+</html>
---
+++ EshtryFrontend/index.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ecommerce
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                string img = "Images/jeans.jpg";
+                string name = "Tailord Jeans";
+                float price = 19.99f;
+                string description = "Some text....";
+                string id = "Button" + i.ToString();
+
+
+                StringBuilder myStringBuilder = new StringBuilder();
+                myStringBuilder.Append("<div class='card'>");
+                myStringBuilder.Append($"<img src='{img}' style='width:100%'/>");
+                myStringBuilder.Append($"<h1>{name}</h1>");
+                myStringBuilder.Append($"<p class='price'>{price.ToString()}</p>");
+                myStringBuilder.Append($"<p>{description}</p>");
+                myStringBuilder.Append($"<p>");
+                pnl.Controls.Add(new LiteralControl(myStringBuilder.ToString()));
+
+
+                Panel mychild = new Panel();
+                Button btn = new Button();
+                btn.Text = "Add to Cart";
+                btn.ID = $"{id}";
+                btn.CssClass = "button";
+                mychild.Controls.Add(btn);
+                mychild.Visible = true;
+                btn.Click += new EventHandler(btn_Click);
+                pnl.Controls.Add(mychild);
+
+
+                StringBuilder myStringBuilder2 = new StringBuilder();
+                myStringBuilder2.Append($"</p></div>");
+                pnl.Controls.Add(new LiteralControl(myStringBuilder2.ToString()));
+                pnl.Visible = true;
+            }
+        }
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            Response.Write(btn.ID);
+
+        }
+    }
+}
